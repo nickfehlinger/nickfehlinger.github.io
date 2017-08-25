fe1842248cb1e9a24154f1834ee22f66e484ce3e
Nick Fehlinger <nfehlinger@gmail.com>
2017-08-25 14:15:49 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var controller = new ScrollMagic.Controller();
+
+	$('.project').each(function(){
+		var projects = new ScrollMagic.Scene({
+			triggerElement: this,
+			duration: '70%',
+			triggerHook: .7
+		})
+		.setClassToggle(this, 'fade-in')
+		.addIndicators()
+		.addTo(controller);
+	})
+});
